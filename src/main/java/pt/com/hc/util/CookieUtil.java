c2ace5c356458bf507aafa93cdb8b955365b6376
André Cleiser <andrecleiser@gmail.com>
2020-10-17 16:32:02 +0100

    teste

--- src/main/java/pt/com/hc/util/CookieUtil.java
+++ src/main/java/pt/com/hc/util/CookieUtil.java
@@ -25,7 +25,7 @@ public class CookieUtil {
         List<NewCookie> listaCookies = new ArrayList<>();
 
         listaCookies.add(new NewCookie(NOME_TOKEN_ACESSO, tokenAcesso.getTokenAcesso(), "/", 
-            null, null, tempoVida, cookieSeguro, false));
+            "app-gestao-associado-frontend.herokuapp.com", null, tempoVida, cookieSeguro, false));
 
         // listaCookies.add(new NewCookie("refreshToken", tokenAcesso.getTokenAcesso(), "/",
         //     null, null, EXPIRA_EM_MINUTOS, cookieSeguro, true));
